00529378411f313a47c3f7de17f84efe53bccfee
Tiberiu Covaci <tibi@covaci.se>
2016-07-25 23:24:42 +0100

    Create the starting app

--- NotesApp/src/app.ts
+++ NotesApp/src/app.ts
@@ -1,1 +1,14 @@
-﻿export class App { }
+import {Router, RouterConfiguration} from 'aurelia-router';
+
+export class App {
+    router: Router;
+
+    configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'Aurelia Notes';
+        config.map([
+            { route: '', moduleId: 'welcome' }
+        ]);
+
+        this.router = router;
+    }
+}
