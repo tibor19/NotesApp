ad15bc7d77399fb3c4aca9f595509faa72c69228
Tiberiu Covaci <tibi@covaci.se>
2016-07-25 23:24:42 +0100

    Create the starting app

--- NotesApp/src/app.ts
+++ NotesApp/src/app.ts
@@ -1,4 +1,7 @@
+<<<<<<< 74f72690c7dd1cc8c2e585512687ba731a831eee
 <<<<<<< b2a5f65ea67d62c801e3ee1f0220f68b5085e8a4
+=======
+>>>>>>> Create the starting app
 import {Router, RouterConfiguration} from 'aurelia-router';
 
 export class App {
@@ -13,6 +16,9 @@ export class App {
         this.router = router;
     }
 }
+<<<<<<< 74f72690c7dd1cc8c2e585512687ba731a831eee
 =======
 ﻿export class App { }
 >>>>>>> Add support for Aurelia
+=======
+>>>>>>> Create the starting app
