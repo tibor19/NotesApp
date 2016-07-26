6768e716fc8253fc6994c3d0868cb9a9b3c4a7d1
Tiberiu Covaci <tibi@covaci.se>
2016-07-26 11:55:10 +0100

    Add dummy definitions file

--- NotesApp/src/app.ts
+++ NotesApp/src/app.ts
@@ -1,7 +1,3 @@
-<<<<<<< 74f72690c7dd1cc8c2e585512687ba731a831eee
-<<<<<<< b2a5f65ea67d62c801e3ee1f0220f68b5085e8a4
-=======
->>>>>>> Create the starting app
 import {Router, RouterConfiguration} from 'aurelia-router';
 
 export class App {
@@ -16,9 +12,3 @@ export class App {
         this.router = router;
     }
 }
-<<<<<<< 74f72690c7dd1cc8c2e585512687ba731a831eee
-=======
-﻿export class App { }
->>>>>>> Add support for Aurelia
-=======
->>>>>>> Create the starting app
